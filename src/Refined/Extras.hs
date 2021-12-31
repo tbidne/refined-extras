542c75e6cb9e1efa20e1dcae6bd34445f32d4f42
Tommy Bidne <tbidne@protonmail.com>
2021-12-31 14:05:45 +1300

    Add module that exports everything

---
+++ src/Refined/Extras.hs
@@ -0,0 +1,17 @@
+-- | Entry point for the library.
+--
+-- @since 0.1.0.0
+module Refined.Extras
+  ( module Refined.Extras.Polymorphism,
+    module Refined.Extras.Predicates.Foldable,
+    module Refined.Extras.Predicates.Text,
+    module Refined.Extras.Unsafe,
+    module Refined.Extras.Utils,
+  )
+where
+
+import Refined.Extras.Polymorphism
+import Refined.Extras.Predicates.Foldable
+import Refined.Extras.Predicates.Text
+import Refined.Extras.Unsafe
+import Refined.Extras.Utils
