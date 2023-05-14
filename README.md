726c91203ff5b25fd4c289a2d33fb3fa05704538
Tommy Bidne <tbidne@protonmail.com>
2023-05-14 13:02:51 +1200

    Simplify ci

--- README.md
+++ README.md
@@ -3,12 +3,8 @@
 # Refined Extras
 
 [![GitHub release (latest SemVer)](https://img.shields.io/github/v/release/tbidne/refined-extras?include_prereleases&sort=semver)](https://github.com/tbidne/refined-extras/releases/)
-![haskell](https://img.shields.io/static/v1?label=&message=9.6&logo=haskell&logoColor=655889&labelColor=2f353e&color=655889)
+[![ci](http://img.shields.io/github/actions/workflow/status/tbidne/refined-extras/ci.yaml?branch=main)](https://github.com/tbidne/refined-extras/actions/workflows/ci.yaml)
 [![BSD-3-Clause](https://img.shields.io/github/license/tbidne/refined-extras?color=blue)](https://opensource.org/licenses/BSD-3-Clause)
-
-[![nix](http://img.shields.io/github/actions/workflow/status/tbidne/refined-extras/nix.yaml?branch=main&label=nix&logo=nixos&logoColor=85c5e7&labelColor=2f353c)](https://github.com/tbidne/refined-extras/actions/workflows/nix.yaml)
-[![cabal](http://img.shields.io/github/actions/workflow/status/tbidne/refined-extras/cabal.yaml?branch=main&label=cabal&labelColor=2f353c)](https://github.com/tbidne/refined-extras/actions/workflows/cabal.yaml)
-[![style](http://img.shields.io/github/actions/workflow/status/tbidne/refined-extras/style.yaml?branch=main&label=style&logoColor=white&labelColor=2f353c)](https://github.com/tbidne/refined-extras/actions/workflows/style.yaml)
 
 </div>
 
--- flake.nix
+++ flake.nix
@@ -6,49 +6,52 @@
   };
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nix-hs-utils = {
+    url = "github:tbidne/nix-hs-utils";
+    inputs.flake-compat.follows = "flake-compat";
+  };
   outputs =
     inputs@{ flake-compat
     , flake-parts
     , nixpkgs
+    , nix-hs-utils
     , self
     }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       perSystem = { pkgs, ... }:
         let
-          buildTools = c: [
-            c.cabal-install
-            pkgs.gnumake
-            pkgs.zlib
-          ];
-          devTools = c: [
-            (hlib.dontCheck c.ghcid)
-            (hlib.dontCheck c.haskell-language-server)
-          ];
-          formatters = c: [
-            (hlib.dontCheck c.cabal-fmt)
-            (hlib.dontCheck c.hlint)
-            (hlib.dontCheck c.ormolu)
-            pkgs.nixpkgs-fmt
-          ];
           hlib = pkgs.haskell.lib;
           ghc-version = "ghc944";
-          compiler = pkgs.haskell.packages."${ghc-version}";
-          mkPkg = returnShellEnv: withDevTools:
-            compiler.developPackage {
-              inherit returnShellEnv;
+          compiler = pkgs.haskell.packages."${ghc-version}".override {
+            overrides = final: prev: {
+              apply-refact = prev.apply-refact_0_11_0_0;
+              # https://github.com/ddssff/listlike/issues/23
+              ListLike = hlib.dontCheck prev.ListLike;
+            };
+          };
+          mkPkg = returnShellEnv:
+            nix-hs-utils.mkHaskellPkg {
+              inherit compiler pkgs returnShellEnv;
               name = "refined-extras";
               root = ./.;
-              modifier = drv:
-                pkgs.haskell.lib.addBuildTools drv
-                  (buildTools compiler ++
-                    (if returnShellEnv then formatters compiler else [ ]) ++
-                    (if withDevTools then devTools compiler else [ ]));
             };
+          hs-dirs = "src test";
         in
         {
-          packages.default = mkPkg false false;
-          devShells.default = mkPkg true true;
-          devShells.ci = mkPkg true false;
+          packages.default = mkPkg false;
+          devShells.default = mkPkg true;
+
+          apps = {
+            format = nix-hs-utils.format {
+              inherit compiler hs-dirs pkgs;
+            };
+            lint = nix-hs-utils.lint {
+              inherit compiler hs-dirs pkgs;
+            };
+            lint-refactor = nix-hs-utils.lint-refactor {
+              inherit compiler hs-dirs pkgs;
+            };
+          };
         };
       systems = [
         "x86_64-darwin"
